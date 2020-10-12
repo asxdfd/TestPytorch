f2ae460fa8d72e4c5a96753cf4f6e96e668b9939
asxdfd <16301170@bjtu.edu.cn>
2020-10-13 00:20:48 +0800

    face detection

---
+++ TestPytorch/include/TestPytorch.h
@@ -0,0 +1,14 @@
+﻿// TestPytorch.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include "torch/script.h"
+#include "torch/nn.h"
+#include "torch/autograd.h"
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+
+// TODO: Reference additional headers your program requires here.
